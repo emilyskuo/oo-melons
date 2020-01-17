306e36b82d79620bb0d531984a60b97a3c34abae
Your Name <you@example.com>
2020-01-17 13:01:48 -0800

    incorporated christmas melon and international shipping fee for intl orders with less than 10 melons

--- melons.py
+++ melons.py
@@ -11,33 +11,29 @@ class AbstractMelonOrder():
         self.qty = qty
         self.shipped = False
 
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+
+        if "christmas" in self.species.lower():
+            base_price = base_price * 1.5
+
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
 
         self.shipped = True
+
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
     order_type = "domestic"
     tax = 0.08
-
-    # def __init__(self, species, qty):
-    #     """Initialize melon order attributes."""
-
-    #     self.species = species
-    #     self.qty = qty
-    #     self.shipped = False
-    #     self.order_type = "domestic"
-    #     self.tax = 0.08
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -49,23 +45,19 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
         super().__init__(species, qty)
-        # self.species = species
-        # self.qty = qty
         self.country_code = country_code
-        # self.shipped = False
-        # self.order_type = "international"
-        # self.tax = 0.17
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        if self.qty < 10:
+            total = super().get_total() + 3
+        else:
+            total = super().get_total()
+
+        return total
