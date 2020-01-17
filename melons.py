f86a51ede1c8d9d516fc0165020030ab63057db8
Your Name <you@example.com>
2020-01-17 15:57:18 -0800

    finish main lab and part of further study for oo-melons

--- melons.py
+++ melons.py
@@ -1,4 +1,6 @@
 """Classes for melon orders."""
+from random import randint
+import datetime
 
 
 class AbstractMelonOrder():
@@ -11,10 +13,23 @@ class AbstractMelonOrder():
         self.qty = qty
         self.shipped = False
 
+    def get_base_price(self):
+        """Get base price of melon based on Splurge Pricing"""
+
+        base_price = randint(5, 9)
+        today = datetime.datetime.now()
+        hour = today.hour
+        day_of_week = datetime.date.weekday(today)
+
+        if day_of_week in range(0, 5) and hour in range(8, 11):
+            base_price += 4
+
+        return base_price
+
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        base_price = self.get_base_price()
 
         if "christmas" in self.species.lower():
             base_price = base_price * 1.5
@@ -55,9 +70,41 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def get_total(self):
         """Calculate price, including tax."""
 
+        # total = super().get_total()
+
+        # if self.qty < 10:
+        #     return total + 3
+
+        # return total
+
         if self.qty < 10:
             total = super().get_total() + 3
         else:
             total = super().get_total()
 
         return total
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A government melon order."""
+
+    order_type = "government"
+    tax = 0
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+
+        super().__init__(species, qty)
+        self.passed_inspection = False
+
+    def mark_inspection(self, passed):
+        """Mark melon as inspected if passed inspection."""
+
+        if passed is True:
+            self.passed_inspection = True
+
+
+class TooManyMelonsError(ValueError):
+    """Raise error when melon quantity is greater than 100."""
+
+    pass
